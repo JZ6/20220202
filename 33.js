336b250639b5222ae4b83863dc931be390153937
JZ <yjzuo6@gmail.com>
2022-02-03 00:01:13 -0500

    init tampermonkey script

---
+++ 33.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://gitlab.com/-/profile/preferences
+// @icon         https://www.google.com/s2/favicons?domain=gitlab.com
+// @grant        none
