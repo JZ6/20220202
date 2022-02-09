b901c74e8e68362e43dfce934b751d12bd9f5d28
JZ <yjzuo6@gmail.com>
2022-02-08 21:23:48 -0500

    add config

--- 33.js
+++ 33.js
@@ -1,9 +1,21 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Gitlab TOTP
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://gitlab.com/-/profile/preferences
+// @description  Bros
+// @author       JZ6
+// @match        *://gitlab.com/*
 // @icon         https://www.google.com/s2/favicons?domain=gitlab.com
 // @grant        none
+// @require      https://raw.githubusercontent.com/Caligatio/jsSHA/master/dist/sha1.js
+// ==/UserScript==
+
+const config = {
+    TOTPKey: "",
+    autoSubmit: false,
+    localStorageKey: "bros",
+    entry: init
+}
+
+config.entry()
+
